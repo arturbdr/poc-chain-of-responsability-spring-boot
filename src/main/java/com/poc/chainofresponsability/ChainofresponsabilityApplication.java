22f6a0fce3f709e1a05c34eb08ef93f40c190430
Artur Drummond <arturbdr@gmail.com>
2018-06-04 15:20:01 -0300

    initial commit

---
+++ src/main/java/com/poc/chainofresponsability/ChainofresponsabilityApplication.java
@@ -0,0 +1,12 @@
+package com.poc.chainofresponsability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainofresponsabilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChainofresponsabilityApplication.class, args);
+    }
+}
